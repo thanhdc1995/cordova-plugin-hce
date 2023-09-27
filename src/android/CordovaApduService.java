7e231ab694aa94d75804d5b9815a19b6ac6b8654
Thanhdc <thanhdc@bap.jp>
2023-09-27 14:39:21 +0700

    update src/android/CordovaApduService.java

--- src/android/CordovaApduService.java
+++ src/android/CordovaApduService.java
@@ -16,7 +16,8 @@ public class CordovaApduService extends HostApduService {
     private static HCEPlugin hcePlugin;
     private static CordovaApduService cordovaApduService;
 
-    static void setHCEPlugin(HCEPlugin _hcePlugin) {
+    public void setHCEPlugin(HCEPlugin _hcePlugin) {
+        cordovaApduService = this;
         hcePlugin = _hcePlugin;
     }
 
@@ -30,23 +31,30 @@ public class CordovaApduService extends HostApduService {
     }
 
     /**
-     * This method will be called when a command APDU has been received from a remote device. A
-     * response APDU can be provided directly by returning a byte-array in this method. In general
-     * response APDUs must be sent as quickly as possible, given the fact that the user is likely
+     * This method will be called when a command APDU has been received from a
+     * remote device. A
+     * response APDU can be provided directly by returning a byte-array in this
+     * method. In general
+     * response APDUs must be sent as quickly as possible, given the fact that the
+     * user is likely
      * holding his device over an NFC reader when this method is called.
      *
-     * <p class="note">If there are multiple services that have registered for the same AIDs in
-     * their meta-data entry, you will only get called if the user has explicitly selected your
+     * <p class="note">
+     * If there are multiple services that have registered for the same AIDs in
+     * their meta-data entry, you will only get called if the user has explicitly
+     * selected your
      * service, either as a default or just for the next tap.
      *
-     * <p class="note">This method is running on the main thread of your application. If you
+     * <p class="note">
+     * This method is running on the main thread of your application. If you
      * cannot return a response APDU immediately, return null and use the {@link
      * #sendResponseApdu(byte[])} method later.
      *
      * @param commandApdu The APDU that received from the remote device
-     * @param extras A bundle containing extra data. May be null.
-     * @return a byte-array containing the response APDU, or null if no response APDU can be sent
-     * at this point.
+     * @param extras      A bundle containing extra data. May be null.
+     * @return a byte-array containing the response APDU, or null if no response
+     *         APDU can be sent
+     *         at this point.
      */
     @Override
     public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
@@ -66,8 +74,10 @@ public class CordovaApduService extends HostApduService {
     }
 
     /**
-     * Called if the connection to the NFC card is lost, in order to let the application know the
-     * cause for the disconnection (either a lost link, or another AID being selected by the
+     * Called if the connection to the NFC card is lost, in order to let the
+     * application know the
+     * cause for the disconnection (either a lost link, or another AID being
+     * selected by the
      * reader).
      *
      * @param reason Either DEACTIVATION_LINK_LOSS or DEACTIVATION_DESELECTED
@@ -90,7 +100,7 @@ public class CordovaApduService extends HostApduService {
      * @return String, containing hexadecimal representation.
      */
     public static String ByteArrayToHexString(byte[] bytes) {
-        final char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        final char[] hexArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
         char[] hexChars = new char[bytes.length * 2]; // Each byte has two hex characters (nibbles)
         int v;
         for (int j = 0; j < bytes.length; j++) {
